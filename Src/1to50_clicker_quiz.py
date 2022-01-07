0213a8dd4ad6171369fe62ddab6eb435755a8594
ahmadbustomi1507 <ahmadbustomi1507@gmail.com>
2022-01-07 11:12:25 +0700

    add an automation for this site "htts://zzzscore.com/1to50/en/"

--- Src/1to50_clicker_quiz.py
+++ Src/1to50_clicker_quiz.py
@@ -5,8 +5,12 @@ from selenium.webdriver.support import expected_conditions as EC
 import logging
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
-driver_path = "E:\Project\PythonSeleniumProject\chromedriver_win32\chromedriver.exe"
+
+
+driver_path = "..\ChromeWebdriver\97.0.4692.71\chromedriver.exe"
 driver      = webdriver.Chrome(executable_path=driver_path)
+
+
 driver.get("https://zzzscore.com/1to50/en/")
 
 for i in range(0,50):
